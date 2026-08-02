c3c8d653fbdf8b35d20af9bf230df1e421577230
ChessNeyshabur <chessneyshabur@example.com>
2026-08-03 02:13:05 +0430

    update

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,28 +1,13 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
- 
 import { createSession } from "@/lib/auth";
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
  
 export async function POST(req: Request) {
   try {
     const { username, password } = await req.json();
  
-    if (!username || !password) {
-      return NextResponse.json(
-        {
-          error: "اطلاعات ناقص است.",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
- 
- 
-    // فقط ادمین مشخص
     if (
-      username.trim() !== "admin" ||
+      username !== "admin" ||
       password !== "Mazda2933"
     ) {
       return NextResponse.json(
@@ -35,60 +20,30 @@ export async function POST(req: Request) {
       );
     }
  
- 
-    // پیدا کردن ادمین از دیتابیس
-    const { data: admin, error } = await supabaseAdmin
-      .from("admins")
-      .select("id")
-      .eq("username", "admin")
-      .single();
- 
- 
-    if (error || !admin) {
-      return NextResponse.json(
-        {
-          error: "ادمین پیدا نشد.",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
- 
- 
-    const token = await createSession(admin.id);
- 
+    // تست بدون Supabase
+    const token = await createSession("123");
  
     const cookieStore = await cookies();
  
-    cookieStore.set(
-      "admin_session",
-      token,
-      {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        path: "/",
-        maxAge: 60 * 60 * 24 * 7,
-      }
-    );
- 
+    cookieStore.set("admin_session", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
  
     return NextResponse.json({
       success: true,
     });
  
- 
-  } catch (error) {
- 
-    console.error(
-      "LOGIN ERROR:",
-      error
-    );
+  } catch (error: any) {
+    console.error("LOGIN ERROR:", error);
  
     return NextResponse.json(
       {
-        error: "SERVER_ERROR",
+        error: error.message,
+        stack: error.stack,
       },
       {
         status: 500,
